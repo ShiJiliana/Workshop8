b0dd3e8aff555ee18d5cf7a9b8f64f882137f505
ShiJiliana <ulgulakova@yandex.ru>
2024-04-10 19:15:37 +0700

    add

--- 5.py
+++ 5.py
@@ -1,22 +1,18 @@
 n = int(input('Введите последнее значение интервала: '))
 dividers = [] #список для делителей
 number_perfect = 0
-p = 0
 def check(a):
-    global p
     dividers.clear()
     for i in range(1, a):
         if a % i == 0:
             dividers.append(i)
     if sum(dividers) == a:
-        p = 1
+        return True
     else:
-        p = 0
-    return p
+        return False
 
 for i in range(2, n + 1):
-    if check(i) == 1:
+    if check(i):
         number_perfect += 1
-    i += 1
 
 print('Кол-во совершенных чисел в интервале =', number_perfect)
