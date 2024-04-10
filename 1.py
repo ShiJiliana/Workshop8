03ba0e6c4c1fbb5182a2bb46757efee008f65691
ShiJiliana <ulgulakova@yandex.ru>
2024-04-10 13:28:59 +0700

    add

---
+++ 1.py
@@ -0,0 +1,15 @@
+b = []
+b_new = []
+n = 0
+for i in range(500):
+    while n != -1:
+        n = int(input(f'Введите результат игрока № {i+1}: '))
+        b.append(n)
+        i += 1
+
+for j in range(len(b) - 1):
+    if b[j] < b[j+1]:
+        b_new.insert(0, b[j+1])
+    j += 1
+
+print('Лучший результат игры:', b_new[0])
