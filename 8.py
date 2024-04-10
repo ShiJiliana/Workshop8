09af1429bcdc875b12047c53bc3d4db79e464069
ShiJiliana <ulgulakova@yandex.ru>
2024-04-10 19:13:42 +0700

    add

--- 8.py
+++ 8.py
@@ -7,7 +7,6 @@ for i in range(2, 10):
     new_number = int(number)
     result = (new_number * 8) + i
     print(number, '* 8 +', j, '=', result)
-    i += 1
 
 number1 = '1'
 r2 = (int(number1)*9) + 2
@@ -16,10 +15,8 @@ for i in range(2, 10):
     j = str(i)
     number1 = number1 + j
     print(number1, '* 9 +', i+1, '=', '1'*(i+1))
-    i += 1
 
 number2 = '1'
 for i in range(1, 10):
     result2 = int((number2 * i))**2
     print(f'{number2*i} * {number2*i} = {result2}')
-    i += 1
